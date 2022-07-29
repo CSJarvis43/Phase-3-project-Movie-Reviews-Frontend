a8c5185228466965771a220b766ba66734922722
Joey Faris <joeyfaris1@yahoo.com>
2022-07-29 11:24:09 -0700

    SearchBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Input } from 'semantic-ui-react'
 
 function Navbar() {
   return (
     <div className="NavBar">
       <center>Welcome Page</center>
         Home Page - Search Bar - Favorites Page
+        <Input className='SearchBar'
+        placeholder='Search Movies' />
     </div>
   )
 }
