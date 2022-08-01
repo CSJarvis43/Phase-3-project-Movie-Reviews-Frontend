b164cdf1370d757ba93ed0bbbefa50c483e607d3
Joey Faris <joeyfaris1@yahoo.com>
2022-08-01 09:19:57 -0700

    rendered movies from backend

--- src/App.js
+++ src/App.js
@@ -2,21 +2,28 @@
 
 
 
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Navbar from './components/NavBar';
 import MoviesCardContainer from './components/MoviesContainer'
 
 function App() {
+  const [movies, setMovies] = useState([])
 
+  useEffect(() => {
+    fetch('http://localhost:9292')
+    .then((response) => response.json())
+    .then((data) => setMovies(data))
+  }, [])
 
-  
+  console.log(movies)
 
   return (
     <div className="App">
       <Navbar 
         />
-      <MoviesCardContainer />
+      <MoviesCardContainer className='MoviesContainer'
+      movies={movies}/>
     </div>
   );
 }
