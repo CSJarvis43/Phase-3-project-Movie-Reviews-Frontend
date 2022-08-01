5ada86b290451b377283d20d8a2515bf2cc16ebc
Charles Jarvis <csjarvis43@gmail.com>
2022-08-01 12:04:09 -0600

    routing functioning and nav bar changed

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/NavBar';
 import MoviesCardContainer from './components/MoviesContainer'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { Container } from '@mui/material';
+import TopTen from './components/TopTen';
+import Favorites from './components/Favorites';
 
 function App() {
 
@@ -13,11 +17,35 @@ function App() {
   
 
   return (
-    <div className="App">
-      <Navbar 
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route 
+          path="/"
+          element={
+            <Container maxWidth={"false"}>
+              <MoviesCardContainer />
+            </Container>
+          }
         />
-      <MoviesCardContainer />
-    </div>
+        <Route 
+          path="/top-ten"
+          element={
+            <Container maxWidth={"false"}>
+              <TopTen />
+            </Container>
+          }
+        />
+        <Route 
+          path="/favorites"
+          element={
+            <Container maxWidth={"false"}>
+              <Favorites />
+            </Container>
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,61 @@
-import React from 'react'
-import { Input } from 'semantic-ui-react'
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import './NavBar.css'
+import { Link as RouterLink } from 'react-router-dom'
 
-function Navbar() {
+const linkBehaviorHome = React.forwardRef((props, ref) => (
+  <RouterLink ref={ref} to="/" {...props} role={undefined} />
+));
+
+const linkBehaviorFav = React.forwardRef((props, ref) => (
+  <RouterLink ref={ref} to="/favorites" {...props} role={undefined} />
+));
+
+const linkBehaviorTop = React.forwardRef((props, ref) => (
+  <RouterLink ref={ref} to="/top-ten" {...props} role={undefined} />
+));
+
+
+
+const NavBar = () => {
+
+
   return (
-    <div className="NavBar">
-      <center>Welcome Page</center>
-        Home Page - Search Bar - Favorites Page
-        <Input className='SearchBar'
-        placeholder='Search Movies' />
-    </div>
-  )
-}
-
-export default Navbar
+    <AppBar position="static" className='navBar'>
+      <Container maxWidth="xl" className='navBarCont'>
+        <Toolbar disableGutters className='toolbar'>
+          <img src="https://i.imgur.com/mY6lXmi.png" alt="logo" id="logoImg"/>
+          <Typography variant="h6" sx={{mx: 1}}>
+            MOVIE REPORT
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+              <Button
+                sx={{ my: 1, mx: 1, color: 'white', display: 'block' }}
+                component={linkBehaviorHome}
+              >
+                Home
+              </Button>
+              <Button
+                sx={{ my: 1, mx: 1, color: 'white', display: 'block' }}
+                component={linkBehaviorFav}
+              >
+                  Favorites
+              </Button>
+              <Button
+                sx={{ my: 1, mx: 1, color: 'white', display: 'block' }}
+                component={linkBehaviorTop}
+              >
+                  Top Movies
+              </Button>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+export default NavBar;
